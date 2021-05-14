01d750c33a00cbdbb2a896ca2c154e7ba3c8d607
efraisse <43862859+efraisse@users.noreply.github.com>
2021-05-14 11:38:25 -0700

    Add files via upload

---
+++ import_agenda.py
@@ -0,0 +1,32 @@
+#import_agenda.py
+import xlrd
+import sys
+
+arg1 = "agenda.xls"
+loc = (arg1) #through jupyter
+#loc = (sys.argv[0]) #through command line arguments
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+table = db_table("table2", {"date": 'text',
+                           "start_time": 'text',
+                          "end_time": 'text',
+                          "session_or_sub": 'text',
+                          "session_title": 'text',
+                          "location": 'text',
+                          "description": 'text',
+                          "speakers": 'text'})
+
+
+index = 15
+while (index < sheet.nrows): #find way to replace 79
+    table.insert({"date": sheet.cell_value(index, 0),
+                  "start_time": sheet.cell_value(index, 1),
+                  "end_time": sheet.cell_value(index, 2),
+                  "session_or_sub": sheet.cell_value(index, 3),
+                  "session_title": sheet.cell_value(index, 4),
+                  "location": sheet.cell_value(index, 5),
+                  "description": sheet.cell_value(index, 6),
+                  "speakers": sheet.cell_value(index, 7)})
+    index += 1
---
+++ lookup_agenda.py
@@ -0,0 +1,40 @@
+#lookup_agenda.py
+import xlrd
+import sys
+
+loc = ("agenda.xls")
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+args1, args2 = "speakers", "Shan Lu" #through jupyter
+#args1, args2 = sys.argv[0], sys.argv[1] #through command line arguments
+
+print(table.select(where = {args1: args2}))
+
+cols = dict()
+cols["date"] = 0
+cols["start_time"] = 1
+cols["end_time"] = 2
+cols["session_or_sub"] = 3
+cols["session_time"] = 4
+cols["location"] = 5
+cols["description"] = 6
+cols["speakers"] = 7
+
+index = 15
+session_index = 0
+while (index < sheet.nrows):
+    if args2 in sheet.cell_value(index, cols[args1]):
+        session_index = index;
+        break;
+    index += 1
+
+
+while (session_index < sheet.nrows):
+    if (sheet.cell_value(session_index + 1, cols["session_or_sub"]) == "Sub"):
+        des = sheet.cell_value(session_index + 1, cols["description"])
+        print(table.select(where = {"description": des})) #I use description because it contains the most characters
+        session_index += 1
+    else:
+        break;
